8e13afaa5f52161c7a27d4dc0c3d3af3ec1dcc28
Renan Almeida <renanalmeida2.ra@gmail.com>
2022-06-07 11:04:01 -0300

    Qk

--- Qk/Qk.cpp
+++ Qk/Qk.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
 int main()
 {
-    //preguiça vo dorme tmj
+    double i =0, n = 1, soma = 0;
+    
+    for(i = 0; i <= 63; i++){
+        
+        n = pow(2,i);
+        
+        soma = + n;
+    
+    }
+    cout<<"Soma total de todos os grãos em cada quadro: " << soma;
+    
+    return 0;
 }
