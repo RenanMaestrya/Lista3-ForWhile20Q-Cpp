ea206ca5badd962f7ec548a7124eaf9eb673ecac
Renan Almeida <renanalmeida2.ra@gmail.com>
2022-06-08 01:58:37 -0300

    commit s question

--- Qs/Qs.cpp
+++ Qs/Qs.cpp
@@ -3,5 +3,29 @@
 using namespace std;
 int main()
 {
-    
+    int dv = 1;
+    int d = 1;
+    int q;
+    int continua;
+
+
+
+    while(dv >= d){
+        
+        cout << "Enter with the dividend: ";
+        cin >> dv;
+            
+        cout << "Enter with the divisor: ";
+        cin >> d;
+
+        q = dv / d;
+        cout << "There are " << q << " divisors in the dividend" << endl;
+
+        cout << "continue? (1 = yes, 0 = no) ";
+        cin >> continua;
+        if(continua == 0){
+            break;
+        }
+    }
+    return 0;
 }
